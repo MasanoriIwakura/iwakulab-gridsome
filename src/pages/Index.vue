78d55accaba6345458557239da35f32a13f3cdca
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-06-04 08:35:50 +0900

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <Layout>
+    
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    
+    <h1>Welcome to IwakuLab !!!</h1>
+   
+    <div>当サイトでは、個人的に気になった技術と知見についてアウトプットします。</div>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'IwakuLab'
+  }
+}
+</script>
